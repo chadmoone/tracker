5390190ef2a3acb72a9ada14869f245fcab552fe
Chad Moone <chadmoone@gmail.com>
2013-07-16 19:22:15 -0400

    Set up dependencies and run rspec generator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tracker::Application.config.secret_key_base = '0a922389d5caacaeb0affc9751d27253cd92098d0ed0d5392b4f22051c9d3d7d4c531203ce09d1b14cadda9f49390b8b9568464190a7e073c535173e35633965'
