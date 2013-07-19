c15e873b28f1e23db92f99fb2e8617922be6e6ba
Chad Moone <chadmoone@gmail.com>
2013-07-18 21:17:25 -0400

    tickets can be edited

---
+++ spec/features/issues/editing_issues_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+feature "Editing Issues" do
+  let!(:project) { FactoryGirl.create(:project) }
+  let!(:issue) { FactoryGirl.create(:issue, project: project) }
+
+  before do
+    visit '/'
+    click_link project.name
+    click_link issue.title
+    click_link "Edit Issue"
+  end
+
+  scenario "Updating an issue" do
+    fill_in "Title", with: "Make it really shiny!"
+    click_button "Update Issue"
+
+    expect(page).to have_content("Issue successfully updated.")
+
+    within("#issue h2") do
+      expect(page).to have_content("Make it really shiny!")
+    end
+
+    expect(page).to_not have_content(issue.title)
+  end
+
+  scenario "Updating an issue with invalid information" do
+    fill_in "Title", with: ""
+    click_button "Update Issue"
+
+    expect(page).to have_content("Issue could not be updated.")
+  end
+end
