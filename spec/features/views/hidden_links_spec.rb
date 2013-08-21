74d1ee89e5276bb7902b935c2423f501112afb59
Chad Moone <chadmoone@gmail.com>
2013-08-21 15:48:22 -0400

    only show links for available actions based on current user

--- spec/features/views/hidden_links_spec.rb
+++ spec/features/views/hidden_links_spec.rb
@@ -4,6 +4,7 @@ feature 'Hidden Links' do
   let(:user) { FactoryGirl.create(:user) }
   let(:admin) { FactoryGirl.create(:admin) }
   let(:project) { FactoryGirl.create(:project) }
+  let(:issue) { FactoryGirl.create(:issue, project: project, user: user) } 
 
   context "anonymous users" do
     scenario 'cannot see the New Project link' do
@@ -41,6 +42,53 @@ feature 'Hidden Links' do
       visit project_path(project)
       assert_no_link_for "Delete Project"
     end
+
+    scenario 'New Issue link is shown to a user with permission' do
+      define_permission!(user, "view", project)
+      define_permission!(user, "create issues", project)
+      visit project_path(project)
+      assert_link_for "New Issue"
+    end
+
+    scenario 'New Issue link is hidden from a user without permission' do
+      define_permission!(user, "view", project)
+      visit project_path(project)
+      assert_no_link_for "New Issue"
+    end
+
+    scenario 'Edit Issue link is shown to a user with permission' do
+      issue
+      define_permission!(user, "view", project)
+      define_permission!(user, "edit issues", project)
+      visit project_path(project)
+      click_link issue.title
+      assert_link_for "Edit Issue"
+    end
+
+    scenario 'Edit Issue link is shown to a user with permission' do
+      issue
+      define_permission!(user, "view", project)
+      visit project_path(project)
+      click_link issue.title
+      assert_no_link_for "Edit Issue"
+    end
+
+    scenario 'Delete Issue link is shown to a user with permission' do
+      issue
+      define_permission!(user, "view", project)
+      define_permission!(user, "delete issues", project)
+      visit project_path(project)
+      click_link issue.title
+      assert_link_for "Delete Issue"
+    end
+
+    scenario 'Delete Issue link is shown to a user with permission' do
+      issue
+      define_permission!(user, "view", project)
+      visit project_path(project)
+      click_link issue.title
+      assert_no_link_for "Delete Issue"
+    end
   end
 
   context "admin users" do
@@ -60,6 +108,25 @@ feature 'Hidden Links' do
     scenario 'can see the Delete Project link' do
       visit project_path(project)
       assert_link_for "Delete Project"
+    end
+
+    scenario 'New Issue link is shown to admins' do
+      visit project_path(project)
+      assert_link_for "New Issue"
+    end
+
+    scenario 'Edit Issue link is shown to admins' do
+      issue
+      visit project_path(project)
+      click_link issue.title
+      assert_link_for "Edit Issue"
+    end
+
+    scenario 'Delete Issue link is shown to admins' do
+      issue
+      visit project_path(project)
+      click_link issue.title
+      assert_link_for "Edit Issue"
     end
   end
 end
