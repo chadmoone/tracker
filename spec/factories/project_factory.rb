3986658e539795e3958eeed117ede8c20eacb359
Chad Moone <chadmoone@gmail.com>
2013-08-27 17:47:03 -0400

    use blocks in factories to force faker to regenerate and prevent collision

--- spec/factories/project_factory.rb
+++ spec/factories/project_factory.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :project do
-    name "#{Faker::Company.name} #{Faker::Company.suffix}"
-    description Faker::Lorem.sentence
+    name { "#{Faker::Company.name} #{Faker::Company.suffix}" }
+    description { Faker::Lorem.sentence }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,15 +2,15 @@
 
 FactoryGirl.define do
   factory :user do
-    firstname Faker::Name.first_name
-    lastname  Faker::Name.last_name
-    email     Faker::Internet.email
+    firstname { Faker::Name.first_name }
+    lastname  { Faker::Name.last_name }
+    email     { Faker::Internet.email }
     password  "password"
 
     factory :admin , class: User do
-      firstname Faker::Name.first_name
-      lastname  Faker::Name.last_name
-      email     Faker::Internet.email
+      firstname { Faker::Name.first_name }
+      lastname  { Faker::Name.last_name }
+      email     { Faker::Internet.email }
       password  "password"
       admin true
     end
